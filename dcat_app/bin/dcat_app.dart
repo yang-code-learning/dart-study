c8487ea419b031060bc47d2005ef0d6bd6135f8f
yangchenleon <913428775@qq.com>
2021-11-01 15:41:20 +0800

    file operation

--- dcat_app/bin/dcat_app.dart
+++ dcat_app/bin/dcat_app.dart
@@ -6,12 +6,14 @@ import 'package:args/args.dart';
 const lineNumber = 'line-number';
 
 void main(List<String> arguments) {
+  getEnvInfo();
+  writeFile1();
+  writeFile2();
   exitCode = 0; // presume success
   final parser = ArgParser()..addFlag(lineNumber, negatable: false, abbr: 'n');
 
   ArgResults argResults = parser.parse(arguments);
   final paths = argResults.rest;
-
   dcat(paths, showLineNumbers: argResults[lineNumber] as bool);
 }
 
@@ -45,4 +47,26 @@ Future<void> _handleError(String path) async {
   } else {
     exitCode = 2;
   }
+}
+
+Future<void> writeFile1() async {
+  final quotes = File('quotes.txt');
+  const stronger = 'That which does not kill us makes us stronger. -Nietzsche';
+
+  await quotes.writeAsString(stronger, mode: FileMode.append);
+}
+Future<void> writeFile2() async {
+  final quotes = File('quotes.txt').openWrite(mode: FileMode.append);
+
+  quotes.write("Don't cry because it's over, ");
+  quotes.writeln('smile because it happened. -Dr. Seuss');
+  await quotes.close();
+}
+
+Future<void> getEnvInfo() async {
+  final envVarMap = Platform.environment;
+
+  print('PWD = ${envVarMap['PWD']}');
+  print('LOGNAME = ${envVarMap['LOGNAME']}');
+  print('PATH = ${envVarMap['PATH']}');
 }
